25444b9be0d97627863606985ea98f7ba4867154
Tobifi98 <andentobias@gmail.com>
2023-05-10 12:51:17 +0200

    keine ahnung achive model und was in packages

---
+++ src/app/shared/achive.model.ts
@@ -0,0 +1,16 @@
+export interface Achive {
+    data:{ 
+        sensor1:[ 
+            { 
+                date: Date, 
+                data: number 
+            } 
+        ], 
+        sensor2:[ 
+            { 
+                date: Date, 
+                data: number 
+            } 
+        ] 
+    }
+  }
