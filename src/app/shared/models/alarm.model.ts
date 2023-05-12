67cc7a4f2942184ae52d315b3e3fe98d3656d8ff
Tobifi98 <andentobias@gmail.com>
2023-05-12 11:31:40 +0200

    irgendwas

--- src/app/shared/models/alarm.model.ts
+++ src/app/shared/models/alarm.model.ts
@@ -7,9 +7,6 @@ export interface Alarm {
                 {
                     code: string,
                     msg: string
-                },
-                {
-
                 }
             ]
         }
