f41d653a8cce2fcb5b9e9d4b805c05f4f0395f37
Robin Kunath <59505945+AnanasHead@users.noreply.github.com>
2023-05-24 08:37:13 +0200

    Last commit before Presentation

--- python/Python_MQTT.py
+++ python/Python_MQTT.py
@@ -1,13 +1,15 @@
 import random
 import time
 from paho.mqtt import client as mqtt_client
+import json
 
-broker = 'kruepv.gibip.de'
-port = 11883
-topic = [("pv/QPIGS/status/ac_input_voltage/#",1),("pv/QPIGS/status/ac_output_apparent_power/#",1)]
+broker = 'test.mosquitto.org' #'kruepv.gibip.de'
+port = 1883 #11883
+topic = [("Industrie4/Palettenlager/#",1),("Industrie4/Farbsensor/#",1)]
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 data = {}
+values = {}
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -27,11 +29,18 @@ def subscribe(client: mqtt_client):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic `{msg.qos}` qos")
         node = msg.topic
         node = node.split('/', 4)
+        keyVal = node[-1]
+
+        try:
+            values[keyVal].append(keyVal, msg.payload.decode())
+        except:
+            values[keyVal] = [keyVal, msg.payload.decode()]
+
         node = node[-2]
         try:
-            data[node].append(msg.payload.decode())
+            data[node].append(values[keyVal])
         except:
-            data[node] = [msg.payload.decode()]
+            data[node] = [values[keyVal]]
         
 
     client.subscribe(topic)
@@ -41,9 +50,10 @@ def run():
     client = connect_mqtt()
     subscribe(client)
     client.loop_start()
-    time.sleep(60)   
+    time.sleep(30)   
     client.disconnect()
     client.loop_stop()
+    print(data)
     return data
 
 if __name__ == '__main__':
